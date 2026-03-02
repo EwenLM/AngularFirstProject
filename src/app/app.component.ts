dec0b8d0e117bfb436f561584ba39f50fedc2011
EwenLM <ewenqglm@gmail.com>
2026-03-02 11:12:35 +0100

    add button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,21 @@ import { CaracteristiquesComponent } from './caracteristiques/caracteristiques.c
   selector: 'app-root',
   imports: [RacesComponent, CaracteristiquesComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-	title = 'Course de Chevaux';
-	numberOfUsers = 146;
-	public name: string ='Olivier';
+  title = 'Course de Chevaux';
+  numberOfUsers = 146;
+  public name: string = 'Olivier';
+
+  isAuth = false;
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000);
+  }
+  onAllumer() {    
+	console.log('On allume tout !');
+}
 
 }
